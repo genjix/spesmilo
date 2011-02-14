f458fc450639f7d233a561b0e8845b339b8a8713
genjix <genjix@l.(none)>
2011-02-14 07:59:29 +0000

    possible french?

---
+++ il8n/fr_FR.ts
@@ -0,0 +1,183 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>Cashier</name>
+    <message>
+        <location filename="cashier.py" line="142"/>
+        <source>Your address:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="cashier.py" line="171"/>
+        <source>Spesmilo</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="cashier.py" line="215"/>
+        <source>Balance: %.2f BTC</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="cashier.py" line="221"/>
+        <source>Send</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="cashier.py" line="222"/>
+        <source>Send bitcoins to another person</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="cashier.py" line="225"/>
+        <source>New address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="cashier.py" line="226"/>
+        <source>Create new address for accepting bitcoins</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="cashier.py" line="229"/>
+        <source>Copy address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="cashier.py" line="230"/>
+        <source>Copy address to clipboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ConnectingDialog</name>
+    <message>
+        <location filename="main.py" line="27"/>
+        <source>Connecting...</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SendDialog</name>
+    <message>
+        <location filename="send.py" line="20"/>
+        <source>Pay to:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="send.py" line="24"/>
+        <source>Amount:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="send.py" line="27"/>
+        <source>&amp;Send</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="send.py" line="30"/>
+        <source>&amp;Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="send.py" line="37"/>
+        <source>&lt;i&gt;Enter a bitcoin address (e.g. 1A9Pv2PYuZYvfqku7sJxovw99Az72mZ4YH)&lt;/i&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="send.py" line="45"/>
+        <source>Send bitcoins</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="send.py" line="57"/>
+        <source>Invalid address</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="send.py" line="58"/>
+        <source>Invalid address: %s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="send.py" line="67"/>
+        <source>Insufficient balance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="send.py" line="68"/>
+        <source>Balance of %g is too small.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TransactionsTable</name>
+    <message>
+        <location filename="cashier.py" line="42"/>
+        <source>Status</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="cashier.py" line="43"/>
+        <source>Date</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="cashier.py" line="44"/>
+        <source>Transactions</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="cashier.py" line="45"/>
+        <source>Credits</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="cashier.py" line="46"/>
+        <source>Balance</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="cashier.py" line="79"/>
+        <source>Confirmed (%i)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="cashier.py" line="81"/>
+        <source>Processing... (%i)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="cashier.py" line="83"/>
+        <source>Validating... (%i)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="cashier.py" line="99"/>
+        <source>Sent to %s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="cashier.py" line="101"/>
+        <source>Received to %s</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>TrayIcon</name>
+    <message>
+        <location filename="main.py" line="51"/>
+        <source>&amp;Cashier</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="55"/>
+        <source>&amp;Send funds</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="main.py" line="60"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
